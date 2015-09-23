113fdc11fe99571847ccb2b261a43235829dc59d
azgz <azmax.koichi+gz@gmail.com>
2015-09-24 01:19:23 +0900

    カテゴリーページへのリンク追加

--- 8/index.php
+++ 8/index.php
@@ -4,19 +4,19 @@
 <title>Topページ</title>
 </head>
 <body>
-<h2>カテゴリー1</h2>
+<h2><a href="category.php">カテゴリー1</a></h2>
 <ul>
 <li><a href="news.php">カテゴリ1-ニュース1タイトル</a></li>
 <li><a href="news.php">カテゴリ1-ニュース2タイトル</a></li>
 <li><a href="news.php">カテゴリ1-ニュース3タイトル</a></li>
 </ul>
-<h2>カテゴリー2</h2>
+<h2><a href="category.php">カテゴリー2</a></h2>
 <ul>
 <li><a href="news.php">カテゴリ2-ニュース1タイトル</a></li>
 <li><a href="news.php">カテゴリ2-ニュース2タイトル</a></li>
 <li><a href="news.php">カテゴリ2-ニュース3タイトル</a></li>
 </ul>
-<h2>カテゴリー3</h2>
+<h2><a href="category.php">カテゴリー3</a></h2>
 <ul>
 <li><a href="news.php">カテゴリ3-ニュース1タイトル</a></li>
 <li><a href="news.php">カテゴリ3-ニュース2タイトル</a></li>
