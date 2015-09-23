87fd9d7fc445682ea5bb72ae281df40e4d9ed4a6
azgz <azmax.koichi+gz@gmail.com>
2015-09-24 01:20:27 +0900

    カテゴリーページを追加

---
+++ 8/category.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>カテゴリーページ</title>
+</head>
+<body>
+<h2>カテゴリー1</h2>
+<ul>
+<li><a href="news.php">カテゴリ1-ニュース1タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース2タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース3タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース4タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース5タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース6タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース7タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース8タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース9タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース10タイトル</a></li>
+</ul>
+<a href="index.php">Top</a>
+</body>
+</html>
