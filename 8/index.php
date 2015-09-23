64c04f28b52c60b29373ebc8779d3d4a6c1622c4
azgz <azmax.koichi+gz@gmail.com>
2015-09-23 17:12:40 +0900

    ニュースアプリ画面遷移図追加

---
+++ 8/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Topページ</title>
+</head>
+<body>
+<h2>カテゴリー1</h2>
+<ul>
+<li><a href="news.php">カテゴリ1-ニュース1タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース2タイトル</a></li>
+<li><a href="news.php">カテゴリ1-ニュース3タイトル</a></li>
+</ul>
+<h2>カテゴリー2</h2>
+<ul>
+<li><a href="news.php">カテゴリ2-ニュース1タイトル</a></li>
+<li><a href="news.php">カテゴリ2-ニュース2タイトル</a></li>
+<li><a href="news.php">カテゴリ2-ニュース3タイトル</a></li>
+</ul>
+<h2>カテゴリー3</h2>
+<ul>
+<li><a href="news.php">カテゴリ3-ニュース1タイトル</a></li>
+<li><a href="news.php">カテゴリ3-ニュース2タイトル</a></li>
+<li><a href="news.php">カテゴリ3-ニュース3タイトル</a></li>
+</ul>
+</body>
+</html>
---
+++ 8/news.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>ニュース詳細ページ</title>
+</head>
+<body>
+<h2>カテゴリー1</h2>
+<h3>ニュースタイトル</h3>
+<p>ニュース本文はここに入ります。最大で1024文字にしましょう。</p>
+</body>
+</html>
