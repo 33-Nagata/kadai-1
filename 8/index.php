c42430b2109c6fbde255a6fe5bff76abfa28b261
g's teacher <school_affairs@mailgroup.gsacademy.tokyo>
2015-09-25 16:38:15 +0900

    とりあえず、カテゴリーとニュースの表示は完了。リンクはまだ未実装。

--- 8/index.php
+++ 8/index.php
@@ -8,7 +8,7 @@ $category_news_block = "";
 
 // カテゴリーテーブルからカテゴリー一覧を、とりあえずカテゴリーIDの小さい順から取得
 $pdo = new PDO("mysql:host=localhost;dbname=news_app;charset=utf8", "root", "");
-$sql = "SELECT * FROM category ORDER BY category_id DESC";
+$sql = "SELECT * FROM category ORDER BY category_id";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $category_lists = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -20,7 +20,7 @@ foreach($category_lists as $category_row){
 	$news_list = array();
 	$category_id = $category_row["category_id"];
 	$category_name = $category_row["category_name"];
-	$category_news_block = '<h2><a href="category.php">' . $category_name . '</a></h2>';
+	$category_news_block .= '<h2><a href="category.php">' . $category_name . '</a></h2>';
 // 各カテゴリー毎に、ニューステーブルから3件ずつニュースを取得するSQLを発行する
 	$sql = "SELECT * FROM news WHERE category_id = :category_id ORDER BY create_date DESC LIMIT 3";
 	$stmt = $pdo->prepare($sql);
@@ -28,11 +28,14 @@ foreach($category_lists as $category_row){
 	$stmt->execute();
 	$news_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	// var_dump($news_list);
+	
+	$category_news_block .= "<ul>";
 	foreach($news_list as $news){
 // 各ニュースのタイトルを表示する
 		$news_title = $news["news_title"];
-		echo $news_title . "<br>";
+		$category_news_block .= '<li><a href="news.php">' . $news_title . '</li>';
 	}
+	$category_news_block .= "</ul>";
 }
 $pdo = null;
 ?>
@@ -47,6 +50,7 @@ $pdo = null;
 <?php echo $category_news_block ?>
 <!-- PHPはここで出力終了 -->
 
+<!-- 実装完了したので、ダミーはコメントアウト
 <h2><a href="category.php">カテゴリー1</a></h2>
 <ul>
 <li><a href="news.php">カテゴリ1-ニュース1タイトル</a></li>
@@ -65,5 +69,6 @@ $pdo = null;
 <li><a href="news.php">カテゴリ3-ニュース2タイトル</a></li>
 <li><a href="news.php">カテゴリ3-ニュース3タイトル</a></li>
 </ul>
+-->
 </body>
 </html>
